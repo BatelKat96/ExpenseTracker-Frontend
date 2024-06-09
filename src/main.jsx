fbf289a28d8530f4908110412f70fe3a5c7893b2
BatelKat96 <batelkat@gmail.com>
2024-06-09 10:29:50 +0300

    Create basic css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { RootCmp } from './RootCmp.jsx'
+import './assets/style/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RootCmp />
